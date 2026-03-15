79610214754c5d4ae687815436779b08559eefd2
lKGreat <76699420+lKGreat@users.noreply.github.com>
2026-03-15 12:22:42 +0800

    feat: Add multiple benchmark suites for application startup and AutoMapper registration, along with an initial Atlas database file.

---
+++ BenchmarkSuite1/StartupRegistrationBenchmarks.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Reflection;
+using Atlas.Application;
+using AutoMapper;
+using BenchmarkDotNet.Attributes;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.VSDiagnostics;
+
+namespace Atlas.Application.Performance;
+[CPUUsageDiagnoser]
+public class StartupRegistrationBenchmarks
+{
+    private Assembly[] _assemblies = null!;
+    [GlobalSetup]
+    public void Setup()
+    {
+        _assemblies = AppDomain.CurrentDomain.GetAssemblies();
+    }
+
+    [Benchmark]
+    public IServiceCollection AddAtlasApplication()
+    {
+        var services = new ServiceCollection();
+        return services.AddAtlasApplication();
+    }
+
+    [Benchmark]
+    public IServiceCollection AddAutoMapperAllLoadedAssemblies()
+    {
+        var services = new ServiceCollection();
+        services.AddAutoMapper(_assemblies);
+        return services;
+    }
+}
